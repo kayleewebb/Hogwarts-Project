4e43857611e0a61304a00702925c00ae585056b8
Kaylee Webb <kayleewebb@MacBook-Pro-2.local>
2026-02-27 12:37:58 -0600

    watchers

--- src/App.vue
+++ src/App.vue
@@ -74,6 +74,7 @@
 <script setup>
 import {ref} from 'vue'
 import {computed} from 'vue'
+import {watch} from 'vue'
 
 let username = 'Harry'
 let shoppingCartItems = ref([{
@@ -146,6 +147,18 @@ let  subtotal = computed(()=>shoppingCartItems.value.reduce((acc, item)=>acc + i
 let shippingEstimate = computed (()=> (subtotal.value> 10000 ? 100 : 50))
 let taxEstimate = computed (()=> subtotal.value * 0.08)
 let total = computed (()=> subtotal.value + shippingEstimate.value + taxEstimate.value)
+
+watch(
+    shoppingCartItems, 
+    () => {
+        localStorage.setItem(
+            'hogwartsShoppingCart',
+            JSON.stringify(shoppingCartItems.value)
+
+     )
+    }, 
+    {deep: true}
+)
 </script>
 
 <style scoped>
